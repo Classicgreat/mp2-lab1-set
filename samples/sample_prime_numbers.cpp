4a02dfcf82f932f674e7291afebbb5a8aa3f22db
Classicgreat <vlados_classicgreat@mail.ru>
2026-09-14 10:10:18 +0300

    first test

--- samples/sample_prime_numbers.cpp
+++ samples/sample_prime_numbers.cpp
@@ -8,21 +8,20 @@
 #include <iomanip>
 
 // #define USE_SET // Использовать класс TSet,
-                // закоментировать, чтобы использовать битовое поле
+// закоментировать, чтобы использовать битовое поле
 
 #ifndef USE_SET // Использовать класс TBitField
 
-#include "tbitfield.h"
+#include "../include/tbitfield.h"
 
-int main()
-{
+int main() {
   int n, m, k, count;
 
   setlocale(LC_ALL, "Russian");
   cout << "Тестирование программ поддержки битового поля" << endl;
   cout << "             Решето Эратосфена" << endl;
   cout << "Введите верхнюю границу целых значений - ";
-  cin  >> n;
+  cin >> n;
   TBitField s(n + 1);
   // заполнение множества
   for (m = 2; m <= n; m++)
@@ -40,8 +39,7 @@ int main()
   count = 0;
   k = 1;
   for (m = 2; m <= n; m++)
-    if (s.GetBit(m))
-    {
+    if (s.GetBit(m)) {
       count++;
       cout << setw(3) << m << " ";
       if (k++ % 10 == 0)
@@ -52,17 +50,16 @@ int main()
 }
 #else
 
-#include "tset.h"
+#include "../include/tset.h"
 
-int main()
-{
+int main() {
   int n, m, k, count;
 
   setlocale(LC_ALL, "Russian");
   cout << "Тестирование программ поддержки множества" << endl;
   cout << "              Решето Эратосфена" << endl;
   cout << "Введите верхнюю границу целых значений - ";
-  cin  >> n;
+  cin >> n;
   TSet s(n + 1);
   // заполнение множества
   for (m = 2; m <= n; m++)
@@ -72,16 +69,15 @@ int main()
     // если м в s, удаление кратных
     if (s.IsMember(m))
       for (k = 2 * m; k <= n; k += m)
-       if (s.IsMember(k))
-         s.DelElem(k);
+        if (s.IsMember(k))
+          s.DelElem(k);
   // оставшиеся в s элементы - простые числа
   cout << endl << "Печать множества некратных чисел" << endl << s << endl;
   cout << endl << "Печать простых чисел" << endl;
   count = 0;
   k = 1;
   for (m = 2; m <= n; m++)
-    if (s.IsMember(m))
-    {
+    if (s.IsMember(m)) {
       count++;
       cout << setw(3) << m << " ";
       if (k++ % 10 == 0)
